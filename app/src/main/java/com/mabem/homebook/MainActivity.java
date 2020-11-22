3940df7f98398294450e46563dd18dc274821b78
Nur <58932402+Nour0700@users.noreply.github.com>
2020-11-22 17:51:52 +0300

    Connecting to Firestore

--- app/src/main/java/com/mabem/homebook/MainActivity.java
+++ app/src/main/java/com/mabem/homebook/MainActivity.java
@@ -1,8 +1,22 @@
 package com.mabem.homebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
