5027911bfeb895104cf9a768c80c8e9ab518b8d9
Nur <58932402+Nour0700@users.noreply.github.com>
2020-11-28 20:33:15 +0300

    Setting up logging in with google

--- app/src/main/java/com/mabem/homebook/login_fragment/LoginFragment.java
+++ app/src/main/java/com/mabem/homebook/login_fragment/LoginFragment.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
@@ -17,15 +18,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.mabem.homebook.Model.User;
 import com.mabem.homebook.R;
 import com.mabem.homebook.databinding.LoginFragmentBinding;
 
+import java.util.Objects;
 import java.util.zip.Inflater;
 
 public class LoginFragment extends Fragment {
@@ -36,12 +45,16 @@ public class LoginFragment extends Fragment {
     private LoginFragmentBinding loginBinding;
     private FirebaseAuth firebaseAuth;
     private String LOGIN_FRAGMENT_TAG = "Login Fragment";
+    private static final int RC_SIGN_IN = 385;
+    GoogleSignInClient googleSignInClient;
 
     //========================================= Methods
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
     }
+
+    //========================================= Overriding
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -71,6 +84,15 @@ public class LoginFragment extends Fragment {
             loginWithEmail(v);
         });
 
+        loginBinding.logInWithGoogleButton.setOnClickListener(v -> {
+            loginWithGoogle();
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
         // The root must be returned to be displayed on the screen.
         // Make sure this statement stays at the bottom of onCreate function.
         return loginBinding.getRoot();
@@ -82,10 +104,10 @@ public class LoginFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if(currentUser != null){
-            Log.i(LOGIN_FRAGMENT_TAG, "onViewCreated: There is a user logged in; " + currentUser.getEmail());
+            Log.d(LOGIN_FRAGMENT_TAG, "onViewCreated: There is a user logged in; " + currentUser.getEmail());
             Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_mainFragment);
         }else{
-            Log.i(LOGIN_FRAGMENT_TAG, "onViewCreated: There is no user logged in. Start normal. ");
+            Log.d(LOGIN_FRAGMENT_TAG, "onViewCreated: There is no user logged in. Start normal. ");
         }
     }
 
@@ -96,6 +118,28 @@ public class LoginFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Exception exception = task.getException();
+            if (task.isSuccessful()) {
+                try {
+                    Log.d(LOGIN_FRAGMENT_TAG, "onActivityResult: Log in to Google was successful. Next, authenticate with Firebase");
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    Log.w(LOGIN_FRAGMENT_TAG, "onActivityResult: Log in to Google failed", e);
+                }
+            } else {
+                Log.w(LOGIN_FRAGMENT_TAG, exception.toString());
+            }
+        }
+    }
+
+    //========================================= Help Methods
+
     public void loginWithEmail(View view){
         String email = loginBinding.logInEmailEditText.getText().toString().trim();
         String password = loginBinding.logInPasswordEditText.getText().toString().trim();
@@ -104,7 +148,7 @@ public class LoginFragment extends Fragment {
         }else{
             final boolean[] returnValue = {false};
             firebaseAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(getActivity(), task -> {
+                    .addOnCompleteListener(requireActivity(), task -> {
                         if(task.isSuccessful()){
                             Log.i(LOGIN_FRAGMENT_TAG, "loginWithEmail: The user was logged in successfully.");
                             Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_mainFragment);
@@ -115,4 +159,22 @@ public class LoginFragment extends Fragment {
         }
     }
 
+    private void loginWithGoogle() {
+        // We have to sign out from the last client to make the user chose every time.
+        googleSignInClient.signOut();
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(requireActivity(), task -> {
+            if (task.isSuccessful()) {
+                Log.d(LOGIN_FRAGMENT_TAG, "firebaseAuthWithGoogle: Authentication with firebase was successful.");
+                Navigation.findNavController(requireActivity(), R.id.log_in_with_google_button).navigate(R.id.action_loginFragment_to_mainFragment);
+            } else {
+                Log.w(LOGIN_FRAGMENT_TAG, "signInWithCredential:failure", task.getException());
+            }
+        });
+    }
 }
